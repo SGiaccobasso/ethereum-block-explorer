621c0c45d247dfa706daa0eea85b68efbfd43f7f
Santiago <sgiaccobasso@gmail.com>
2021-10-28 17:56:23 -0300

    Retrieve last block numbers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import { useState, useEffect, useCallback } from 'react';
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
 
 function App() {
+  const [latestBlock, setLatestBlock] = useState(0);
+  const [blockList, setBlockList] = useState<number[]>([]);
+
+  const createNewBlocksListener = useCallback(() => {
+    provider.on('block', newBlockNum => {
+      setLatestBlock(newBlockNum);
+    });
+  }, []);
+
+  useEffect(() => {
+    // Create listener to listen for new blocks in the blockchain
+    createNewBlocksListener();
+  }, [createNewBlocksListener]);
+
+  useEffect(() => {
+    if (latestBlock !== 0) {
+      // Create array with the last 10 block numbers
+      const newList = Array.from({ length: 10 }, (_, i) => latestBlock - i);
+      setBlockList(newList);
+    }
+  }, [latestBlock]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {blockList.map(item => (
+        <div key={item}>{item}</div>
+      ))}
     </div>
   );
 }
